cb2cdbe5874df8a37601f0a26dabcaba1dc804c8
Zhao Tang <tangzhao1999@hotmail.com>
2021-06-04 00:11:15 -0700

    more improvement to height mapping

--- src/Application.cpp
+++ src/Application.cpp
@@ -554,19 +554,40 @@ void Application::render(float frametime) {
 }
 
 
-#define THEIGHT  70
-#define TWIDTH  70
-#define TTEX 1 //every unit is divided into ten smaller tex units
-void add_vert_to_terrain(const int &x,const int &z, const float &v_x, const float &v_z, const float &v_y, float*terrain_map, float *terrain_map_density){
-    int index = (x+THEIGHT/2)*TWIDTH + (z+TWIDTH/2);
+#define THEIGHT  60
+#define TWIDTH  60
+#define TTEX 4 //every unit is divided into ten smaller tex units
+void add_vert_to_terrain(const int &x,const int &z, const float &v_x, const float &v_z, const float &v_y, 
+    float*terrain_map, float *terrain_map_density, char* terrain_map_set){
+    int index = (x+THEIGHT*TTEX/2)*TWIDTH*TTEX + (z+TWIDTH*TTEX/2);
     float weight = 1 / ( distance(vec2((float) x, (float) z), vec2(v_x, v_z)) + 0.01);
     
     terrain_map[index] += weight * v_y;
     float prev_w = terrain_map_density[index];
     terrain_map_density[index] += weight;
+    terrain_map_set[index] = 1;
     if(isnan(abs(terrain_map_density[index]))){
         cout<<'\t'<<"weight is nan"<<weight<<' '<<prev_w<<endl;
     }
+}
+
+void average_surrounding(float*terrain_map, int i, int j){
+    int w_min = std::min(0, i-1);
+    int w_max = std::max(THEIGHT*TTEX, i+2);
+    int h_min = std::min(0, j-1);
+    int h_max = std::max(TWIDTH*TTEX, j+2);
+    float sum = 0.0f;
+    int count = 0;
+    for(int u=w_min; u<w_max; u++){
+        for(int v=h_min; v<h_max; v++){
+            sum+=terrain_map[u*TWIDTH*TTEX + v];
+            count ++;
+        }
+    }
+    if(terrain_map[i+j*TWIDTH*TTEX] > 0.0001){
+        count++;
+    }
+    terrain_map[i+j*TWIDTH*TTEX] = (int)(sum/(float)count);
 }
 
 void Application::obj_to_terrain_map(){
@@ -583,34 +604,37 @@ void Application::obj_to_terrain_map(){
     tinyobj::shape_t terrain = terrainV.at(0);
     float* terrain_map = new float[THEIGHT*TTEX*TWIDTH*TTEX];
     float* terrain_map_density = new float[THEIGHT*TTEX*TWIDTH*TTEX];
+    char* terrain_map_set = new char[THEIGHT*TTEX*TWIDTH*TTEX];
+
+
     for(int i=0; i<THEIGHT*TTEX*TWIDTH*TTEX; i++){
         terrain_map[i] = 0.0f;
         terrain_map_density[i] = 0.0001f;
+        terrain_map_set[i] = 0;
     }
     for(auto iter=terrain.mesh.positions.begin(); iter!=terrain.mesh.positions.end(); iter+=3){
-        int l_x = (int)*iter;
-        int l_z = (int)*(iter+2);
-        if(l_x >= - TWIDTH/2 && l_x < (TWIDTH/2-1) && l_z >= - THEIGHT/2 && l_z < (THEIGHT/2-1)){
+        int l_x = (int)((*iter)*TTEX);
+        int l_z = (int)((*(iter+2))*TTEX);
+        if(l_x > - TWIDTH*TTEX/2 && l_x < (TWIDTH*TTEX/2-1) && l_z >= - THEIGHT*TTEX/2 && l_z < (THEIGHT*TTEX/2-1)){
             // cout<<l_x<<' '<<l_z<<endl;
-            add_vert_to_terrain(l_x, l_z, *iter, *(iter+2), *(iter+1), terrain_map, terrain_map_density);
-            add_vert_to_terrain(l_x+1, l_z, *iter, *(iter+2), *(iter+1), terrain_map, terrain_map_density);
-            add_vert_to_terrain(l_x, l_z+1, *iter, *(iter+2), *(iter+1), terrain_map, terrain_map_density);
-            add_vert_to_terrain(l_x+1, l_z+1, *iter, *(iter+2), *(iter+1), terrain_map, terrain_map_density);
+            add_vert_to_terrain(l_x, l_z, *iter, *(iter+2), *(iter+1), terrain_map, terrain_map_density, terrain_map_set);
+            add_vert_to_terrain(l_x+1, l_z, *iter, *(iter+2), *(iter+1), terrain_map, terrain_map_density, terrain_map_set);
+            add_vert_to_terrain(l_x, l_z+1, *iter, *(iter+2), *(iter+1), terrain_map, terrain_map_density, terrain_map_set);
+            add_vert_to_terrain(l_x+1, l_z+1, *iter, *(iter+2), *(iter+1), terrain_map, terrain_map_density, terrain_map_set);
         }
     }
     ofstream terrain_map_file, terrain_map_ppm;
     terrain_map_file.open(resourceDirectory+"/Desert/Dune_hmap.txt");
     terrain_map_ppm.open(resourceDirectory+"/Desert/Dune_hmap.ppm");
     terrain_map_ppm<<"P3"<<endl;
-    terrain_map_ppm<<TWIDTH<<' '<<THEIGHT<<endl;
+    terrain_map_ppm<<TWIDTH*TTEX<<' '<<THEIGHT*TTEX<<endl;
 
     terrain_map_file<<THEIGHT<<' '<<TWIDTH<<endl;
+    terrain_map_file<<TTEX<<endl;
     float min = 0;
     float max = -10;
-    // for(int i=0; i<THEIGHT*TWIDTH; i++){
-    //     cout<<"density"<<terrain_map_density[i]<<endl;
-    // }
-    for(int i=0; i<THEIGHT*TWIDTH; i++){
+    
+    for(int i=0; i<THEIGHT*TTEX*TWIDTH*TTEX; i++){
         float prv = terrain_map[i];
         int prv_w = terrain_map_density[i];
         terrain_map[i]/=terrain_map_density[i];
@@ -618,7 +642,7 @@ void Application::obj_to_terrain_map(){
             cout<<"nan occured "<<terrain_map[i]<<'\t'<<terrain_map_density[i]<<prv<<'\t'<<prv_w<<endl;
         }
         if(abs(terrain_map[i])>100){
-            cout<<"big h occured "<<i/TWIDTH-THEIGHT/2<<' '<<i%TWIDTH-TWIDTH/2<<'\t'<<terrain_map[i]<<'\t'<<prv<<'\t'<<terrain_map_density[i]<<endl;
+            cout<<"big h occured "<<i/(TWIDTH*TTEX)-THEIGHT*TTEX/2<<' '<<i%(TWIDTH*TTEX)-TWIDTH*TTEX/2<<'\t'<<terrain_map[i]<<'\t'<<prv<<'\t'<<terrain_map_density[i]<<endl;
         }
         if(terrain_map[i]>max)
             max = terrain_map[i];
@@ -626,13 +650,25 @@ void Application::obj_to_terrain_map(){
             min = terrain_map[i];
         terrain_map_file<<terrain_map[i]<<endl;
     }
+
+
+    // for(int k=0; k<3; k++){
+        for(int i=0; i<THEIGHT*TTEX; i++){
+            for(int j=0; j<TWIDTH*TTEX; j++){
+                if(terrain_map_set[i*TWIDTH*TTEX + j]==0){
+                    average_surrounding(terrain_map, i, j);
+                }
+            }
+        }
+    // }
+
     cout<<min<<' '<<max<<endl;
     float range_k = 256/(max-min);
     cout<<"range_k"<<range_k<<endl;
     terrain_map_ppm<<255<<endl;
-    for(int i=0; i<TWIDTH; i++){
-        for(int j=0; j<THEIGHT; j++){
-            int h_val = (int) ((terrain_map[i*TWIDTH + j] - min)*range_k);
+    for(int i=0; i<TWIDTH*TTEX; i++){
+        for(int j=0; j<THEIGHT*TTEX; j++){
+            int h_val = (int) ((terrain_map[i + j*TWIDTH*TTEX] - min)*range_k);
             terrain_map_ppm<<h_val<<' '<<h_val<<' '<<h_val<<'\t';
         }
         terrain_map_ppm<<endl;
